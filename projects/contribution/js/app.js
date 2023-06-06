103590fe7449bacf29df2fdaba6a290d102f6aee
Viktor <70236188+monsherok@users.noreply.github.com>
2023-06-07 03:05:20 +0500

    фииикс

--- projects/contribution/js/app.js
+++ projects/contribution/js/app.js
@@ -11,7 +11,7 @@
       return console.error("Failed to fetch contributions:", t), null;
     }
   })().then((t) =>
-    u(
+    s(
       (function (t) {
         const e = {},
           n = Object.keys(t);
@@ -24,28 +24,36 @@
     )
   );
   const t = new Date(),
-    e = new Date(t.getTime() - 308448e5),
-    n = document.createElement("table");
-  let o = null,
-    r = null;
+    e = new Date(
+      (function (t) {
+        const e = t.getFullYear(),
+          n = String(t.getMonth() + 1).padStart(2, "0"),
+          o = String(t.getDate()).padStart(2, "0");
+        return `${e}-${n}-${o}`;
+      })(t)
+    ),
+    n = new Date(e.getTime() - 308448e5),
+    o = document.createElement("table");
+  let r = null,
+    a = null;
   const c = document.createElement("tr");
   for (let t = 0; t <= 51; t++) {
-    const n = document.createElement("th"),
-      a = new Date(e.getTime() + 7 * t * 24 * 60 * 60 * 1e3).toLocaleDateString(
+    const e = document.createElement("th"),
+      o = new Date(n.getTime() + 7 * t * 24 * 60 * 60 * 1e3).toLocaleDateString(
         "ru-RU",
         { month: "short" }
       );
-    0 == t && (o = a),
-      51 != t || o != a
+    0 == t && (r = o),
+      51 != t || r != o
         ? (50 == t &&
-            o == a &&
-            (c.querySelector(`[data-month=${a}]`).textContent = ""),
-          a !== r &&
-            ((n.textContent = a), n.setAttribute("data-month", a), (r = a)),
-          c.appendChild(n))
-        : c.appendChild(n);
+            r == o &&
+            (c.querySelector(`[data-month=${o}]`).textContent = ""),
+          o !== a &&
+            ((e.textContent = o), e.setAttribute("data-month", o), (a = o)),
+          c.appendChild(e))
+        : c.appendChild(e);
   }
-  n.appendChild(c);
+  o.appendChild(c);
   for (let t = 0; t < 7; t++) {
     const t = document.createElement("tr"),
       e = document.createElement("td");
@@ -54,19 +62,19 @@
       const e = document.createElement("td");
       t.appendChild(e);
     }
-    n.appendChild(t);
+    o.appendChild(t);
   }
-  const a = document.querySelector(".squares"),
-    l = document.querySelector(".info");
-  function i(n, o) {
-    const r = new Date(n),
-      c = Math.floor((r.getTime() - e.getTime()) / 864e5);
-    let a = Math.floor(c / 7),
+  const l = document.querySelector(".squares"),
+    i = document.querySelector(".info");
+  function u(t, o) {
+    const r = new Date(t),
+      a = Math.floor((r.getTime() - n.getTime()) / 864e5);
+    let c = Math.floor(a / 7),
       l = r.getDay(),
-      i = t.getDay();
-    0 === l && (l = 7), 0 === i && (i = 7), l <= i && (a += 1);
+      i = e.getDay();
+    0 === l && (l = 7), 0 === i && (i = 7), l < i && (c += 1);
     const u = document.querySelector(
-      "table tr:nth-child(" + (l + 1) + ") td:nth-child(" + (a + 1) + ")"
+      "table tr:nth-child(" + (l + 1) + ") td:nth-child(" + (c + 1) + ")"
     );
     if (!u) return;
     u.querySelector(".tooltip") && u.querySelector(".tooltip").remove();
@@ -100,9 +108,10 @@
             "Декабрь",
           ][e.getMonth()],
           r = e.getDate(),
-          c = e.getFullYear();
-        return `${n}, ${o} ${r}, ${c}`;
-      })(n)}</div>`),
+          a = e.getFullYear(),
+          c = `${n}, ${o} ${r}, ${a}`;
+        return c;
+      })(t)}</div>`),
       u.appendChild(s),
       u.setAttribute(
         "data-level",
@@ -124,13 +133,13 @@
         })(o)
       );
   }
-  function u(t) {
-    for (let e in t) i(e, t[e]);
+  function s(t) {
+    for (let e in t) u(e, t[e]);
   }
-  a.insertBefore(n, l),
-    u(
-      (function (e = 0) {
-        const n = new Date(t.getFullYear() - 1, t.getMonth(), t.getDate()),
+  l.insertBefore(o, i),
+    s(
+      (function (t = 0) {
+        const n = new Date(e.getFullYear() - 1, e.getMonth(), e.getDate()),
           o = {};
         for (let t = 0; t <= 365; t++) {
           const e = new Date(n.getTime() + 24 * t * 60 * 60 * 1e3);
